6a461510dfe9e0498b4fbe4a5f17138009836fce
jok405 <john.y.kim84@gmail.com>
2017-12-05 15:00:11 -0500

    Getting started

---
+++ assets/javascript/iGif.js
@@ -0,0 +1,10 @@
+
+
+
+var danceMoves = ['breaking', 'crip-walking', 'locking', 'popping', 'electric boogaloo', 'floating', 'turfing', 'robot', 'liquids', 'jerkin', 'harlem shake', 'krumping', 'naenae', 'wobble', 'dougie', 'cat daddy', 'stanky legg', 'soulja boy', 'walk it out'];
+
+
+//functions
+$(document).ready(function) {
+	
+}
