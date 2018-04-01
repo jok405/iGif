e4bc8b81331b247a4673c0dae83dea4676462676
jok405 <john.y.kim84@gmail.com>
2018-04-01 19:55:50 -0400

    major rework

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# iGif
-Gify
+# Animal Gif App
+A Webapp that uses jQuery, AJAX and the GIPHY API to display cute animal GIFs of your choosing.
+
+Please checkout out the deployed app on Heroku [here](###http://.herokuapp.com)!
+
+# Screenshots
+
+#### Click on a blue animal button to pull in GIFs from GIPHY 
+![Animal Button](/screenshots/GIF.png)
+
+#### Add a new animal button using the form and press submit 
+![Animal Button](/screenshots/Submit.png)
+
+#### Click on each image to play the GIF
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,49 +1,15 @@
-/*
-
-
-
-//functions
-$(document).ready(function() {
-
-//dance buttons
-	renderButtons();
-	//calling ajax function to show dance giphy on button
-	$(document).on('click', '.dance_button', showDanceGif);
-	//hiding images
-	$(document).on('click', '.gif_container', showGifHideImage);
-
-	function renderButtons(){
-
-		$('#dance_buttons').empty();
-
-		for (var i = 0; i < dances.lenth; i++){
-
-			var danceButton = $('<button>')
-			danceButton.addClass('dance_button'); //added a class
-			danceButton.attr('data-name', dances[i]); //added a data-attribute
-			danceButton.text('dances[i]'); //provides initial button text
-			$('#dance_buttons').append(danceButton); //added the button to the HTML
-		}
-	}
-
-
-
-*/
-
-
-
-// Initial array of danceMoves
-var danceMoves = ['breaking', 'crip-walking', 'locking', 'popping', 'electric boogaloo', 'floating', 'turfing', 'robot', 'liquids', 'jerkin', 'harlem shake', 'krumping', 'naenae', 'wobble', 'dougie', 'cat daddy', 'stanky legg', 'soulja boy', 'walk it out'];
+// Initial array of animals
+const animals = ['cat', 'dog', 'hamster', 'shark', 'frog', 'pig', 'turtle', 'rabbit', 'monkey', 'skunk', 'squirrel', 'hedgehog', 'ferret', 'zebra', 'lion', 'eagle', 'rhino', 'elephant'];
 
 
 // -------------------------- Functions  --------------------------
-$( document ).ready(function(){
+$(document).ready(() => {
 
 	// Append Intial animal buttons
 	renderButtons();
 
 	// Call AJAX function to display animal Giphy on selected animal button
-	$(document).on('click', '.dance_button', showDanceGif);
+	$(document).on('click', '.animal_button', displayAnimalGif);
 
 
 	$(document).on('click', '.gif_container', showGifHideImage);
@@ -51,57 +17,57 @@ $( document ).ready(function(){
 	// ===================================================================
 
 	// Append Animal Buttons to DOM
-	function renderButtons(){ 
+	function renderButtons() {
 
 		// Deletes any previous button to prevent duplicates
-		$('#dance_buttons').empty();
+		$('#animal_buttons').empty();
 
-		// Loops through the array of danceMoves
-		for (var i = 0; i < danceMoves.length; i++){
+		// Loops through the array of animals
+		for (let i = 0; i < animals.length; i++) {
 
 			// Then dynamicaly generate a button for each animal in the array 
-		    var danceButton = $('<button>') 
-		    danceButton.addClass('dance_button'); // Added a class 
-		    danceButton.attr('data-name', danceMoves[i]); // Added a data-attribute
-		    danceButton.text(danceMoves[i]); // Provided the initial button text
-		    $('#dance_buttons').append(danceButton); // Added the button to the HTML
+			const animalButton = $('<button>');
+			animalButton.addClass('animal_button'); // Added a class 
+			animalButton.attr('data-name', animals[i]); // Added a data-attribute
+			animalButton.text(animals[i]); // Provided the initial button text
+			$('#animal_buttons').append(animalButton); // Added the button to the HTML
 		}
 	}
 
 	// ===================================================================
 
-	// Add new danceMoves from the user input
-	$('#add_dance').on('click', function(){
+	// Add new animals from the user input
+	$('#add_animal').on('click', () => {
 
 		// Grab the input from the textbox
-		var newDance = $('#dance_input').val().trim().toLowerCase();
+		const newAnimal = $('#animal_input').val().trim().toLowerCase();
 
 
 		// Validate user input
-		var isUnique = true;
-		for(var i = 0; i < danceMoves.length; i++){
-			if(danceMoves[i] == newDance){
+		let isUnique = true;
+		for (let i = 0; i < animals.length; i++) {
+			if (animals[i] == newAnimal) {
 				isUnique = false;
 			}
 		}
 
 
 		// Append new button if the input is unique
-		if(newDance == ""){
+		if (newAnimal == "") {
 			alert("Sorry. No empty buttons are allowed!")
 		}
-		else if(isUnique){
+		else if (isUnique) {
 
 			// Add the new animal to the original list
-			danceMoves.push(newDance);
-		
-			
+			animals.push(newAnimal);
+
+
 			// Add new buttons to the DOM
 			renderButtons();
 
 		}
-		else{
-			alert("You already have a " + newDance + " button!")
+		else {
+			alert(`You already have a ${newAnimal} button!`)
 		}
 
 
@@ -113,81 +79,81 @@ $( document ).ready(function(){
 	// ===================================================================
 
 	// Collect Animal gif from GIPHY and display it to the DOM when the animal button is clicked
-	function showDanceGif(){
+	function displayAnimalGif() {
 
 		// Deletes old gifs
-		$('#dance_images').empty();
+		$('#animal_images').empty();
 
 		// Collect animal name data attribute from the button, replacing any spaces
-		var dance = $(this).attr('data-name').replace(/ /g, '+');
+		const animal = $(this).attr('data-name').replace(/ /g, '+');
 
 		// Create the API URL
-		var publicKey = "dc6zaTOxFJmzC"; // Public API Key
-		var limit = "10"; // Limit API to 10 gifs
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dance + "&limit=" + limit + "&api_key=" + publicKey;
+		const publicKey = "dc6zaTOxFJmzC"; // Public API Key
+		const limit = "10"; // Limit API to 10 gifs
+		const queryURL = `http://api.giphy.com/v1/gifs/search?q=${animal}&limit=${limit}&api_key=${publicKey}`;
 		//console.log(queryURL);
 
-		
+
 		// Creates AJAX call for the specific animal button being clicked
-		$.ajax({url: queryURL, method: 'GET'}).done(function(response){
+		$.ajax({ url: queryURL, method: 'GET' }).done(response => {
 
 			// Loop through the JSON output to collect each Animal Object
-			for(var i = 0; i < response.data.length; i++){
+			for (let i = 0; i < response.data.length; i++) {
 
 				// Collect the animal gif URLs
-				var currentStillURL = response.data[i].images.fixed_height_still.url; // still image 
-				var currentMovingURL = response.data[i].images.fixed_height.url; // moving image
+				const currentStillURL = response.data[i].images.fixed_height_still.url; // still image 
+				const currentMovingURL = response.data[i].images.fixed_height.url; // moving image
 
 				// Collect the animal gif Ratings
-				var currentRating = response.data[i].rating;
+				let currentRating = response.data[i].rating;
 
-					// Correct for empty rating
-					if(currentRating == ""){
-						currentRating = "none";
-					}
+				// Correct for empty rating
+				if (currentRating == "") {
+					currentRating = "none";
+				}
 
 
 				// Create a Div to house Gif and Rating
-				var currentGifDiv = $('<div>');
+				const currentGifDiv = $('<div>');
 				currentGifDiv.addClass('gif_container'); // Added a class
 				currentGifDiv.attr('data-name', "unclicked"); // Added a Data Attributed for clicked
-				
+
 				// Append Rating to current gif
-				var currentGifRating = $('<h1>');
-				currentGifRating.text("Rating: " + currentRating);
+				const currentGifRating = $('<h1>');
+				currentGifRating.text(`Rating: ${currentRating}`);
 				currentGifDiv.append(currentGifRating);
 
 				// Append Still Image
-				var currentGifImage = $('<img>');
+				const currentGifImage = $('<img>');
 				currentGifImage.addClass('still_gif'); // Added a class for still gif
 				currentGifImage.attr("src", currentStillURL);
 				currentGifDiv.append(currentGifImage);
 
 				// Append Moving Gif Image
-				var currentGif = $('<img>')
+				const currentGif = $('<img>');
 				currentGif.addClass('moving_gif'); // Added a class for animated gif
 				currentGif.attr("src", currentMovingURL);
 				currentGif.hide(); // Hide the moving gif
 				currentGifDiv.append(currentGif);
 
 				// Append current Div to the DOM
-			    $('#dance_images').append(currentGifDiv);
+				$('#animal_images').append(currentGifDiv);
 
 			}
 
-		});	
+		});
 	}
 
 	// ===================================================================
-	
+
 	// Display the Moving gif and Hide the still Image (and vice versa)
-	function showGifHideImage(){
+	function showGifHideImage() {
 
 		// Determine in the gif was already clicked
-		var clickTest = $(this).attr('data-name');
-		
+		const clickTest = $(this).attr('data-name');
+
 		// Gif is not clicked yet - Hide the still image & display the moving image
-		if(clickTest == "unclicked"){
+		if (clickTest == "unclicked") {
 
 			var gifChildren = $(this).children();
 
@@ -202,7 +168,7 @@ $( document ).ready(function(){
 
 		}
 		// Gif was already clicked - Hide the moving image & show the still image
-		else{
+		else {
 
 			var gifChildren = $(this).children();
 
@@ -217,7 +183,7 @@ $( document ).ready(function(){
 			$(this).attr('data-name', "unclicked");
 
 		}
-	
+
 	}
 
 	// ===================================================================
